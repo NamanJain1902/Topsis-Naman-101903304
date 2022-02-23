df4a37b9b093a7bb2878ebb97c0c7c3cbc814e68
NamanJain1902 <1902ben10@gmail.com>
2022-02-24 02:11:45 +0530

    packaged 1.0.1

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setup(
+    name="Topsis-Naman-101903304",
+    version="1.0.1",
+    description="A Python package implementing TOPSIS technique.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Naman Jain",
+    author_email="1902ben10@gmail.com",
+    license="MIT",
+    url='https://github.com/NamanJain1902/Topsis-Naman-101903304',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=["Topsis-Naman-101903304"],
+    include_package_data=True,
+    install_requires=['pandas'],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.topsis:main",
+        ]
+    },
+)
